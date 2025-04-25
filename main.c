774e174e58fd5d39115ce5eb1fe1596166432641
chungkyung <dlcndrud123@geonwoo.com>
2025-04-25 18:39:23 +0900

    짜장면면

--- main.c
+++ main.c
@@ -3,6 +3,6 @@
 int main() {
     printf("Hello Jenkins!\n");
     //출력체크 .
-    //
+    //ㄴㄴ
     return 0;
 }
--- test_main.c
+++ test_main.c
@@ -6,13 +6,13 @@ int main() {
     int result = system("main.exe > output.txt");
 
     if (result != 0) {
-        printf("❌ 실행 실패\n");
+        printf("qqwqe\n");
         return 1;
     }
 
     FILE *fp = fopen("output.txt", "r");
     if (!fp) {
-        printf("❌ 출력 파일 없음\n");
+        printf("asd\n");
         return 1;
     }
 
@@ -21,10 +21,10 @@ int main() {
     fclose(fp);
 
     if (strcmp(buffer, "Hello Jenkins!\n") == 0) {
-        printf("✅ 테스트 통과\n");
+        printf("sexy\n");
         return 0;
     } else {
-        printf("❌ 테스트 실패: 출력값 = %s\n", buffer);
+        printf("sexy12 = %s\n", buffer);
         return 1;
     }
 }
